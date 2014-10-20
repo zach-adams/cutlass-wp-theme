875bfc24697385c1f0b439771009c96417d523ce
Zach Adams <zach@getsocialeyes.com>
2014-10-20 11:17:13 -0600

    Updated 404 template and content

--- 404.php
+++ 404.php
@@ -1,13 +1,7 @@
-<?php get_template_part('templates/page', 'header'); ?>
+@layout( 'templates.layouts.full-width' )
 
-<div class="alert alert-warning">
-  <?php _e('Sorry, but the page you were trying to view does not exist.', 'cutlass'); ?>
-</div>
+@section('page-content')
 
-<p><?php _e('It looks like this was the result of either:', 'cutlass'); ?></p>
-<ul>
-  <li><?php _e('a mistyped address', 'cutlass'); ?></li>
-  <li><?php _e('an out-of-date link', 'cutlass'); ?></li>
-</ul>
+	@include('templates.content.404')
 
-<?php get_search_form(); ?>
+@endsection
---
+++ templates/content/404.blade.php
@@ -0,0 +1,5 @@
+@include('templates.includes.page-header')
+
+<h1>Sorry there doesn't appear to be content here!</h1>
+<p>Try searching for what you're looking for:</p>
+{{ get_search_form() }}
