c80c586bec0fd85993f66b6618b7aa6233a9a557
Zach Adams <zach@getsocialeyes.com>
2014-10-20 14:29:51 -0600

    Removed irrelevant function

--- lib/config.php
+++ lib/config.php
@@ -28,39 +28,6 @@ function cutlass_sidebar_body_class($classes) {
 add_filter('body_class', 'cutlass_sidebar_body_class');
 
 /**
- * Define which pages shouldn't have the sidebar
- *
- * See lib/sidebar.php for more details
- */
-function cutlass_display_sidebar() {
-  $sidebar_config = new cutlass_Sidebar(
-    /**
-     * Conditional tag checks (http://codex.wordpress.org/Conditional_Tags)
-     * Any of these conditional tags that return true won't show the sidebar
-     *
-     * To use a function that accepts arguments, use the following format:
-     *
-     * array('function_name', array('arg1', 'arg2'))
-     *
-     * The second element must be an array even if there's only 1 argument.
-     */
-    array(
-      'is_404',
-      'is_front_page'
-    ),
-    /**
-     * Page template checks (via is_page_template())
-     * Any of these page templates that return true won't show the sidebar
-     */
-    array(
-      'template-custom.php'
-    )
-  );
-
-  return apply_filters('cutlass/display_sidebar', $sidebar_config->display);
-}
-
-/**
  * $content_width is a global variable used by WordPress for max image upload sizes
  * and media embeds (in pixels).
  *
