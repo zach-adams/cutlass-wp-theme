c8f2485feacdeee78782a536e7430f23fa473a04
Zach Adams <zach@getsocialeyes.com>
2014-10-20 14:31:36 -0600

    Removed irrelevant theme supports

--- lib/config.php
+++ lib/config.php
@@ -2,9 +2,6 @@
 /**
  * Enable theme features
  */
-add_theme_support('soil-clean-up');         // Enable clean up from Soil
-add_theme_support('soil-relative-urls');    // Enable relative URLs from Soil
-add_theme_support('soil-nice-search');      // Enable /?s= to /search/ redirect from Soil
 add_theme_support('bootstrap-gallery');     // Enable Bootstrap's thumbnails component on [gallery]
 add_theme_support('jquery-cdn');            // Enable to load jQuery from the Google CDN
 
