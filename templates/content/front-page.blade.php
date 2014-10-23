ffb6ee68a5969120c4800357827314060b85b5ee
zach-adams <zach@getsocialeyes.com>
2014-10-23 16:05:52 -0600

    Front Page now by default shows posts correctly

--- templates/content/front-page.blade.php
+++ templates/content/front-page.blade.php
@@ -1,6 +1,17 @@
 @wpposts
-	{{ the_content() }}
-	{{ wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')) }}
+  @include('templates.content.excerpt')
 @wpempty
-	<h1>It's Empty!</h1>
+  <div class="alert alert-warning">
+    {{ _e('Sorry, no results were found.', 'cutlass') }} 
+  </div>
+  {{ get_search_form(false) }}
 @wpend
+
+@if ($wp_query->max_num_pages > 1)
+  <nav class="post-nav">
+    <ul class="pager">
+      <li class="previous">{{ next_posts_link(__('&larr; Older posts', 'cutlass')) }}</li>
+      <li class="next">{{ previous_posts_link(__('Newer posts &rarr;', 'cutlass')) }}</li>
+    </ul>
+  </nav>
+@endif
