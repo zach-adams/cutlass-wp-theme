298324a170e76b32f2895add8912d690dbb80939
zach-adams <zach@getsocialeyes.com>
2014-10-23 15:52:01 -0600

    Sidebar layout is no longer full-width by default

--- templates/layouts/sidebar.blade.php
+++ templates/layouts/sidebar.blade.php
@@ -1,10 +1,12 @@
 @layout('templates.layouts.base')
 
 @section('content')
-  <main class="main sidebar" role="main">
-    @yield('page-content')
-  </main><!-- /.main -->
-  <aside class="sidebar" role="complementary">
-    <?php dynamic_sidebar('sidebar-primary'); ?>
-  </aside><!-- /.sidebar -->
+	<div class="container">
+	  <main class="main sidebar" role="main">
+	    @yield('page-content')
+	  </main><!-- /.main -->
+	  <aside class="sidebar" role="complementary">
+	    <?php dynamic_sidebar('sidebar-primary'); ?>
+	  </aside><!-- /.sidebar -->
+	</div>
 @endsection
