1a84e7865dc9c49855f2ab8492527234748d62de
zach-adams <zach@getsocialeyes.com>
2014-10-23 15:41:39 -0600

    Switched out Nav Walker for better support

--- functions.php
+++ functions.php
@@ -8,17 +8,17 @@
  * Please note that missing files will produce a fatal error.
  */
 $cutlass_includes = array(
-  'inc/blade.php',           // Load Laravel's Blade Templating Engine
-  'inc/utils.php',           // Utility functions
-  'inc/init.php',            // Initial theme setup and constants
-  'inc/config.php',          // Configuration
-  'inc/activation.php',      // Theme activation
-  'inc/titles.php',          // Page titles
-  'inc/nav.php',             // Custom nav modifications
-  'inc/gallery.php',         // Custom [gallery] modifications
-  'inc/comments.php',        // Custom comments modifications
-  'inc/scripts.php',         // Scripts and stylesheets
-  'inc/extras.php'           // Custom functions
+  'inc/blade.php',                  // Load Laravel's Blade Templating Engine
+  'inc/utils.php',                  // Utility functions
+  'inc/init.php',                   // Initial theme setup and constants
+  'inc/config.php',                 // Configuration
+  'inc/activation.php',             // Theme activation
+  'inc/titles.php',                 // Page titles
+  'inc/wp_bootstrap_navwalker.php', // Bootstrap Nav Walker (From https://github.com/twittem/wp-bootstrap-navwalker)
+  'inc/gallery.php',                // Custom [gallery] modifications
+  'inc/comments.php',               // Custom comments modifications
+  'inc/scripts.php',                // Scripts and stylesheets
+  'inc/extras.php'                  // Custom functions
 );
 
 foreach ($cutlass_includes as $file) {
