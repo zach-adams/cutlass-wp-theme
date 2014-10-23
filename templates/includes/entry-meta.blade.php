55a301232cfaf1ba8bc6ddc69227c72bfb88dfcb
zach-adams <zach@getsocialeyes.com>
2014-10-23 16:30:45 -0600

    Meta is now on one line and padding is decreased

--- templates/includes/entry-meta.blade.php
+++ templates/includes/entry-meta.blade.php
@@ -1,2 +1,4 @@
-<time class="published" datetime="{{ get_the_time('c') }}">{{ get_the_date() }}</time>
-<p class="byline author vcard">{{ __('By', 'cutlass') }} <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">{{ get_the_author() }}</a></p>
+<p class="byline author vcard">
+	<time class="published" datetime="{{ get_the_time('c') }}">{{ get_the_date() }}</time>
+	{{ __('By', 'cutlass') }} <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">{{ get_the_author() }}</a>
+</p>
