866581c294a742f78ae20bf4ca7490b82ea4413c
zach-adams <zach@zach-adams.com>
2015-06-26 18:21:14 -0600

    Added get_posts method and better docs

--- inc/CutlassHelper.php
+++ inc/CutlassHelper.php
@@ -7,7 +7,19 @@
  */
 class CutlassHelper {
 
+	/**
+	 * get_title
+	 *
+	 * Returns a nice formatted title according to which page
+	 * we're on.
+	 *
+	 * From Root's Sage
+	 * https://github.com/roots/sage
+	 *
+	 * return @string
+	 */
 	public static function get_title() {
+
 		if (is_home()) {
 			if (get_option('page_for_posts', true)) {
 				return get_the_title(get_option('page_for_posts', true));
@@ -23,5 +35,32 @@ class CutlassHelper {
 		} else {
 			return get_the_title();
 		}
+
+	}
+
+	/**
+	 * get_posts
+	 *
+	 * Checks global wp_query for posts and returns them,
+	 * otherwise runs get_posts on passed query
+	 *
+	 * @param array $query
+	 * @return array
+	 */
+	public static function get_posts($query = array()) {
+
+		$posts = array();
+
+		if( empty($query) ) {
+			global $wp_query;
+
+			if( property_exists($wp_query->posts) && !empty($wp_query->posts) )
+				$posts = $wp_query->posts;
+		} else {
+			$posts = get_posts($query);
+		}
+
+		return $posts;
+
 	}
 }
