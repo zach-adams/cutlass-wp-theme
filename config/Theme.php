73519a0f4131289e652e0ce44d017fc409b66c4d
zach-adams <zach@zach-adams.com>
2015-07-08 17:17:19 -0600

    Added post type to body class page slug

--- config/Theme.php
+++ config/Theme.php
@@ -94,7 +94,7 @@ function cutlass_body_class($classes) {
 	// Add page slug if it doesn't exist
 	if (is_single() || is_page() && !is_front_page()) {
 		if (!in_array(basename(get_permalink()), $classes)) {
-			$classes[] = basename(get_permalink());
+			$classes[] = get_post_type() . '-' . basename(get_permalink());
 		}
 	}
 	return $classes;
