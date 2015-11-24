c6443c3c122eb411b54e6c91c4af6324f1d2081c
Zach <zach@zach-adams.com>
2015-11-24 10:03:05 -0700

    Fixed date displaying wrong, fixed author link so it's a little nicer

--- resources/views/includes/entry-meta.blade.php
+++ resources/views/includes/entry-meta.blade.php
@@ -1,6 +1,4 @@
-@inject('carbon', 'Carbon\Carbon')
-
 <p class="byline author vcard">
-	<time class="published" datetime="{{ $carbon->now() }}">{{ $post->human_date }}</time>
-	{{ __('By', 'cutlass') }} <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">{{ $post->author->data->display_name }}</a>
+	<time class="published" datetime="{{ $post->date }}">{{ $post->human_date }}</time>
+	By <a href="{{ get_author_posts_url($post->author->data->ID) }}" rel="author" class="fn">{{ $post->author->data->display_name }}</a>
 </p>
