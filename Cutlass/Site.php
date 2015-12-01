eca77394d7c7476d124e8ac3ab543afe27cafcb0
Zach <zach@zach-adams.com>
2015-12-01 08:40:14 -0700

    Added search_form helper

--- Cutlass/Site.php
+++ Cutlass/Site.php
@@ -179,4 +179,67 @@ class Site
 
     }
 
+
+    /**
+     * Gets the WordPress comments template using the comments_template function
+     *
+     * Will not display the comments template if not on single post or page, or if
+     * the post does not have comments.
+     *
+     * Uses the WordPress database object to query for the comments. The comments
+     * are passed through the 'comments_array' filter hook with the list of comments
+     * and the post ID respectively.
+     *
+     * The $file path is passed through a filter hook called, 'comments_template'
+     * which includes the TEMPLATEPATH and $file combined. Tries the $filtered path
+     * first and if it fails it will require the default comment template from the
+     * default theme. If either does not exist, then the WordPress process will be
+     * halted. It is advised for that reason, that the default theme is not deleted.
+     *
+     * @param string     $file
+     * @param bool|false $separate_comments
+     *
+     * @return void
+     */
+    public function comments( $file = '/comments.php', $separate_comments = false )
+    {
+
+        comments_template($file, $separate_comments);
+
+    }
+
+
+    /**
+     * Gets the WordPress search form using the get_search_form function
+     *
+     * Will first attempt to locate the searchform.php file in either the child or
+     * the parent, then load it. If it doesn't exist, then the default search form
+     * will be displayed. The default search form is HTML, which will be displayed.
+     * There is a filter applied to the search form HTML in order to edit or replace
+     * it. The filter is 'get_search_form'.
+     *
+     * This function is primarily used by themes which want to hardcode the search
+     * form into the sidebar and also by the search widget in WordPress.
+     *
+     * There is also an action that is called whenever the function is run called,
+     * 'pre_get_search_form'. This can be useful for outputting JavaScript that the
+     * search relies on or various formatting that applies to the beginning of the
+     * search. To give a few examples of what it can be used for.
+     *
+     * @param bool|true $echo
+     *
+     * @return string|void String when $echo is false.
+     */
+    public function search_form($echo = true)
+    {
+
+        if ( $echo ) {
+            echo get_search_form($echo);
+        }
+        else {
+            return get_search_form($echo);
+        }
+
+    }
+
 }
