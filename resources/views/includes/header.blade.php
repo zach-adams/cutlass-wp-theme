61127276737456495584c21c52291074eefda7da
Zach <zach@zach-adams.com>
2015-12-01 08:32:43 -0700

    New menu and has_menu helpers

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -2,8 +2,8 @@
 	<a href="{{ $site->info('url') }}">{{ $site->info('name') }}</a>
 
 	<nav id="primary-navigation" role="navigation">
-			@if(has_nav_menu('primary_navigation'))
-				{{ wp_nav_menu([
+			@if($site->has_menu('primary_navigation'))
+				{{ $site->menu([
 					'menu'              => 'primary_navigation',
 					'theme_location'    => 'primary_navigation',
 				]) }}
