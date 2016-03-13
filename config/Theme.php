2e0f0fa8d60f86834ddf3df5caf35effbe6a1ac2
Zach <zach@zach-adams.com>
2016-03-13 15:50:54 -0600

    remove soil

--- config/Theme.php
+++ config/Theme.php
@@ -35,13 +35,6 @@ function cutlass_setup()
 	 */
 	add_theme_support( 'title-tag' );
 
-	add_theme_support('soil-clean-up');
-	add_theme_support('soil-nav-walker');
-	add_theme_support('soil-relative-urls');
-	add_theme_support('soil-js-to-footer');
-	add_theme_support('soil-disable-trackbacks');
-	add_theme_support('soil-disable-asset-versioning');
-
 	//Register default nav menus
 	register_nav_menus([
 			'primary_navigation' => 'Primary Navigation'
